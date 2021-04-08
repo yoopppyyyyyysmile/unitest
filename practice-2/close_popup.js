45ac733e48598c5144d8f1f67d978e4eaf144341
yoopppyyyyyysmile <37539443+yoopppyyyyyysmile@users.noreply.github.com>
2021-04-08 13:44:33 +0300

    Delete practice-2 directory

--- practice-2/close_popup.js
+++
@@ -1,7 +0,0 @@
-const popupCloseIcon = document.getElementsByClassName(popup__close);
-if (popupCloseIcon.length > 0) {
-    el.addEventListener('click', function (e) {
-        popupClose(el.close('.popup'));
-        e.preventDefault();
-    });
-}
