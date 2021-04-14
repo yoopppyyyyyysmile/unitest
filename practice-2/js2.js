4557240bd26dcb18c818fd1a5cb71febf135d07a
yoopppyyyyyysmile <yoopppyyyyyysmile@gmail.com>
2021-04-14 13:38:44 +0300

    изменила функцию buildTable и изменила получение json

--- practice-2/js2.js
+++ practice-2/js2.js
@@ -1,17 +1,26 @@
 document.getElementById('get').addEventListener('click', () => {
-    let table = document.getElementsByClassName('post');
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(result => {
-            console.log(result); 
-            return result.json();
-        })
-        .then(get => {
-            table.innerHTML = `User id ${post.userId} id ${post.id} title ${post.title} body ${post.body}`
-        })
-});
+    let table = document.querySelector('table');
+    let requestURL = 'https://jsonplaceholder.typicode.com/posts';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        let jsonObj = request.response;
+        buildTable(jsonObj);
+    }
 
-
-
-
-            
- 
+    function buildTable(jsonObj) {
+      
+        for (let i = 0; i < jsonObj.length; i++) {
+          let row = `<tr>
+                      <td>${jsonObj[i].userId}</td>
+                      <td>${jsonObj[i].id}</td>
+                      <td>${jsonObj[i].title}</td>
+                      <td>${jsonObj[i].body}</td>
+                      </tr>`
+      
+            table.innerHTML += row
+        }
+      }
+    });
