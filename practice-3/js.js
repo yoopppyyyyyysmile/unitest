29cef368a01779e13f0ea0e9cd8c4d227319bc49
yoopppyyyyyysmile <yoopppyyyyyysmile@gmail.com>
2021-04-14 13:44:22 +0300

    Update js.js

--- practice-3/js.js
+++ practice-3/js.js
@@ -6,45 +6,30 @@
 
 getResponse()*/
 
- let jsonData = [
-            {
-                "userId": 1,
-                "id": 1,
-                "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-                "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-              },
-              {
-                "userId": 1,
-                "id": 2,
-                "title": "qui est esse",
-                "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-              },
-              {
-                "userId": 1,
-                "id": 3,
-                "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-                "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-              }
-    ]
+let table = document.querySelector('table');
+    let requestURL = 'https://jsonplaceholder.typicode.com/posts';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        let jsonObj = request.response;
+        buildTable(jsonObj);
+    }
 
-   
-
-    buildTable(jsonData)
-
-function buildTable(data) {
-  let table = document.getElementById('myTable')
-
-  for (let i = 0; i < data.length; i++) {
-    let row = `<tr>
-                <td>${data[i].userId}</td>
-                <td>${data[i].id}</td>
-                <td>${data[i].title}</td>
-                <td>${data[i].body}</td>
-                </tr>`
-
-      table.innerHTML += row
-  }
-}
+    function buildTable(jsonObj) {
+      
+        for (let i = 0; i < jsonObj.length; i++) {
+          let row = `<tr>
+                      <td>${jsonObj[i].userId}</td>
+                      <td>${jsonObj[i].id}</td>
+                      <td>${jsonObj[i].title}</td>
+                      <td>${jsonObj[i].body}</td>
+                      </tr>`
+      
+            table.innerHTML += row
+        }
+      }
 
 document.getElementsByTagNameNS('td').addEventListener('click', function() {
   console.log('Column was cliked')
